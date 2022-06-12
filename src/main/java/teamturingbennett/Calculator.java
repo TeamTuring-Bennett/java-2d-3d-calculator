03387e7a529912d4a4b342be62bff53f0f72db55
= <=>
2022-06-13 01:52:53 +0530

    final changes to the entire project

---
+++ src/main/java/teamturingbennett/Calculator.java
@@ -0,0 +1,38 @@
+/*
+    Calculator.java
+    Main Application class
+ */
+package teamturingbennett;
+
+import java.io.File;
+import java.net.URL;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+
+public class Calculator extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        URL url = new File("src/main/resources/teamturingbennett/main.fxml").toURI().toURL();
+        Parent root = FXMLLoader.load(url);
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setMinWidth(700);
+        stage.setMinHeight(900);
+        stage.setResizable(true);
+        stage.initStyle(StageStyle.UNIFIED);
+        stage.setTitle("Calculator");
+        //stage.getIcons().add(new Image(getClass().getResource("src/main/resources/teamturingbennett/Calculator-icon.png").toExternalForm()));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/teamturingbennett/StringFunction.java
+++
@@ -1,7 +0,0 @@
-package teamturingbennett;
-
-import java.util.*;
-
-public class StringFunction {
-    
-}
